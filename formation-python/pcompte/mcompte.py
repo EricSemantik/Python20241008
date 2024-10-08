a8d6cb80f7ff6a218b01256b0a3303387d578c9d
Eric SULTAN <e.sultan@ajc-ingenierie.fr>
2024-10-08 15:11:45 +0200

    Exercice 31

--- formation-python/pcompte/mcompte.py
+++ formation-python/pcompte/mcompte.py
@@ -3,10 +3,20 @@ from typing import NoReturn
 
 class Compte :
 
+    __nbr_comptes : int = 0
+
     def __init__(self, numero: int, solde: float):
         self.__numero : int = numero
         self.__solde : float = solde
         self.__titulaire : Client = None
+        Compte.__nbr_comptes += 1
+
+    def __del__(self) :
+        Compte.__nbr_comptes -= 1
+
+    @classmethod
+    def get_nbr_comptes ( cls ) :
+        return cls.__nbr_comptes
 
     @property
     def numero(self) -> int:
