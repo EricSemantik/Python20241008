8cbff6b751d1a431e76d3e4f0240505a7107be70
Eric SULTAN <e.sultan@ajc-ingenierie.fr>
2024-10-09 13:35:40 +0200

    Exercice 34 starter

--- formation-python/pclient/mclient.py
+++ formation-python/pclient/mclient.py
@@ -2,10 +2,15 @@ from typing import NoReturn
 
 class Client :
 
-    def __init__(self, email: str, nom : str = "", prenom : str = ""):
+    def __init__(self, id: int = None ,email: str, nom : str = "", prenom : str = ""):
+        self.__id = id
         self.__email = email
         self.__nom = nom
         self.__prenom = prenom
+
+    @property
+    def id(self) -> str:
+        return self.__id
 
     @property
     def email(self) -> str:
@@ -19,6 +24,14 @@ class Client :
     def prenom(self) -> str:
         return self.__prenom
 
+    @id.setter
+    def id(self, id: int) -> NoReturn:
+        self.__id = id
+
+    @email.setter
+    def email(self, email: str) -> NoReturn:
+        self.__email = email
+
     @nom.setter
     def nom(self, nom: str) -> NoReturn:
         self.__nom = nom
@@ -26,3 +39,9 @@ class Client :
     @prenom.setter
     def prenom(self, prenom: str) -> NoReturn:
         self.__prenom = prenom
+
+    def __str__(self) -> str:
+        return f"{self.id} {self.nom} {self.prenom} [{self.email}]"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
--- formation-python/pcompte/mcompte.py
+++ formation-python/pcompte/mcompte.py
@@ -7,7 +7,12 @@ class Compte :
 
     __nbr_comptes : int = 0
 
-    def __init__(self, numero: int, solde: float, date_ouverture: date = date.today()):
+    @classmethod
+    def get_nbr_comptes ( cls ) :
+        return cls.__nbr_comptes
+
+    def __init__(self, id: int = None, numero: int, solde: float, date_ouverture: date = date.today()):
+        self.__id : int = id
         self.__numero : int = numero
         self.__solde : float = solde
         self.__titulaire : Client = None
@@ -15,11 +20,11 @@ class Compte :
         Compte.__nbr_comptes += 1
 
     def __del__(self) :
-        Compte.__nbr_comptes -= 1
+        Compte.__nbr_comptes -= 1    
 
-    @classmethod
-    def get_nbr_comptes ( cls ) :
-        return cls.__nbr_comptes
+    @property
+    def id(self) -> int:
+        return self.__id
 
     @property
     def numero(self) -> int:
@@ -50,11 +55,16 @@ class Compte :
 
         self.__solde -= montant
 
+    def __str__(self) -> str: 
+        return f"{self.numero} {self.solde}  {self.date_ouverture} - {self.titulaire}"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
 
 
 class CompteEpargne (Compte):
-    def __init__(self, numero: int, solde: float, date_ouverture: date = date.today(), taux_interet: float = 0.2, duree_blocage : int = 5):
-        super().__init__(numero, solde, date_ouverture)
+    def __init__(self, id: int, numero: int, solde: float, date_ouverture: date = date.today(), taux_interet: float = 0.2, duree_blocage : int = 5):
+        super().__init__(id, numero, solde, date_ouverture)
         self.__taux_interet = taux_interet
         self.__duree_blocage = duree_blocage
 
@@ -86,3 +96,9 @@ class CompteEpargne (Compte):
             raise BanqueBlocageException(f"Durée écoulée : {duree_ecoulee} - Durée de blocage : {self.duree_blocage}")
 
         super().debiter(montant)
+
+    def __str__(self) -> str :
+        return f"{super().__str__()} - {self.taux_interet} {self.duree_blocage}"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
